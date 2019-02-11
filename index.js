70a38824afdab4c0b64c7d976c84467839ab07a1
Richard Dowland <rdowland@hotmail.co.uk>
2019-02-11 16:33:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,5 +18,16 @@ function whisper(string) {
 
 function logShout(string) {
   
+  uppercase = string.toUpperCase()
+  string = uppercase;
+  console.log(uppercase);
+  
+}
+
+function logWhisper(string) {
+  
+  lowercase = string.toLowerCase()
+  string = uppercase;
+  console.log(lowercase);
   
 }
